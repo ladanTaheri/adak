70b23674981e5a19882d09ddd810a4f6064c2f8c
ladan taheri <taheriprogrammer110@gmail.com>
2021-01-07 15:50:56 +0330

    test

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head';
 import Home from '../components/home/Home';
 
 export default function Index() {
+
   return (
     <div>
       <Head>
         <title>اداک</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Home/>
+      <Home />
     </div>
   )
 }
