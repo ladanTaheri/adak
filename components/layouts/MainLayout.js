0d0cd42a14ba6c9084a09e41101c3c56b3466b73
ladan taheri <45038844+ladanTaheri@users.noreply.github.com>
2021-11-06 12:38:51 +0330

    Update MainLayout.js

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import Footer from '../common/footer/Footer';
-import Nav from '../common/nav/Nav';
+import Navig from '../common/nav/Nav';
 class MainLayout extends Component {
     state = {}
     render() {
         return (
             <Fragment>
-                <Nav/>
+                <Navig/>
                 {this.props.children}
                 <Footer/>
             </Fragment>
