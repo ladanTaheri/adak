c7bfde17964d07d8431ba4173c0eba3479ed17c7
ladan taheri <45038844+ladanTaheri@users.noreply.github.com>
2021-11-08 18:43:26 +0330

    Update Blog.js

--- components/blog/Blog.js
+++ components/blog/Blog.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import styles from './Blog.module.css';
-import Item from './item/Item';
+import React, { Component } from "react";
+import styles from "./Blog.module.css";
+import Item from "./item/Item";
 class Blog extends Component {
-    state = {}
-    render() {
-        return (
-            <section className={styles.main}>
-               <Item imgUrl="/images/blog/blog2.jpg"/>
-               <Item imgUrl="/images/blog/blog3.jpg"/>
-            </section>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div className={styles.cardWrapper}>
+        <Item imgUrl="/images/blog/blog2.jpg" />
+        <Item imgUrl="/images/blog/blog3.jpg" />
+        <Item imgUrl="/images/blog/blog3.jpg" />
+      </div>
+    );
+  }
 }
 
 export default Blog;
