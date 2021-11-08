bd7af89ff99598d9b963fe728166568319933298
ladan taheri <45038844+ladanTaheri@users.noreply.github.com>
2021-11-08 21:13:57 +0330

    dynamic item.js

--- components/blog/item/Item.js
+++ components/blog/item/Item.js
@@ -1,26 +1,20 @@
 import React, { Component } from "react";
 import styles from "../Blog.module.css";
 import { Fragment } from "react";
-import Link from 'next/link';
+import Link from "next/link";
+import config from '../../../services/config.json';
 
 class Item extends Component {
-  state = {
-    brief: true,
-  };
-  handleClick = () => {
-    this.setState({ brief: !this.state.brief });
-  };
   render() {
-      const item ={
-          name:"عنوان"
-      }
     return (
       <Fragment>
         <div className={styles.card}>
-            <img src={this.props.imgUrl} className={styles.image}/>
-            <h1 className={styles.heading}>عنوان</h1>
-            <p className={styles.shortText}> کوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتکوتاه متن اخباکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارراه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبارن اخبارکوتاه متن اخبارکوتاه متن اخبارکوتاه متن اخبار</p>
-            <Link href='/detail/[name]' as={`/detail/${item.name}`}><a  className={styles.btn}>مشاهده بیشتر</a></Link>
+          <img src={`${config.api}${this.props.data.image.url}`} className={styles.image} />
+          <h1 className={styles.heading}>{this.props.data.title}</h1>
+          <p className={styles.shortText}>{this.props.data.description}</p>
+          <Link href="/detail/[name]" as={`/detail/${this.props.data.id}`}>
+            <a className={styles.btn}>مشاهده بیشتر</a>
+          </Link>
         </div>
       </Fragment>
     );
