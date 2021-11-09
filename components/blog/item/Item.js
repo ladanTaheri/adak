a04a8c8c435929ff90067c42fe388c9d82986b17
ladan taheri <45038844+ladanTaheri@users.noreply.github.com>
2021-11-09 17:10:00 +0330

    Update Item.js

--- components/blog/item/Item.js
+++ components/blog/item/Item.js
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 import styles from "../Blog.module.css";
 import { Fragment } from "react";
 import Link from "next/link";
-import config from '../../../services/config.json';
+import config from "../../../services/config.json";
+import moment from "moment-jalaali";
 
 class Item extends Component {
   render() {
     return (
       <Fragment>
         <div className={styles.card}>
-          <img src={`${config.api}${this.props.data.image.url}`} className={styles.image} />
+          <img
+            src={`${config.api}${this.props.data.image.url}`}
+            className={styles.image}
+          />
           <h1 className={styles.heading}>{this.props.data.title}</h1>
           <p className={styles.shortText}>{this.props.data.description}</p>
-          <Link href="/detail/[name]" as={`/detail/${this.props.data.id}`}>
-            <a className={styles.btn}>مشاهده بیشتر</a>
-          </Link>
+          <div className={styles.footer}>
+            <Link href="/detail/[name]" as={`/detail/${this.props.data.id}`}>
+              <a className={styles.btn}>مشاهده بیشتر</a>
+
+            </Link>
+            {/* <p>{moment(this.props.data.published_at, 'YYYY-M-D HH:mm:ss').format('YYYY/M/D')} </p> */}
+            <p className={styles.para}> تاریخ:{moment(this.props.data.published_at, 'YYYY-M-D HH:mm:ss').endOf('jMonth').format('jYYYY/jM/jD')} --  نویسنده : اداک</p>
+            {/* <span>{this.props.data.published_at}</span> */}
+          </div>
         </div>
       </Fragment>
     );
